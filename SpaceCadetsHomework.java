62b9ef0c6c80ad5bfe6dea0c8d799088a2719b04
AlexandruAmarandei <aas1u16@soton.ac.uk>
2016-10-05 13:42:00 +0100

    Update SpaceCadetsHomework.java

--- SpaceCadetsHomework.java
+++ SpaceCadetsHomework.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package spacecadetshomework;
+
+package spacecadetsChallange1;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -17,14 +13,10 @@ import java.net.*;
  *
  * @author aas1u16
  */
-public class SpaceCadetsHomework {
+public class SpaceCadetsChallange1 {
 
-    /**
-     * @param args the command line arguments
-     * @throws java.io.IOException
-     */
     public static void main(String[] args) throws IOException {
-        // TODO code application logic here
+        
         
         BufferedReader reader = null;
         reader = new BufferedReader ( new InputStreamReader(System.in));
